4465fa9f0e101cd4b1fd70a33f7a423992cbd82d
zeinputra <kurniaadika5@gmail.com>
2021-08-21 00:33:06 +0700

    adikaputra

---
+++ dashtreme-master/resources/views/calendar.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+
+@section('title', 'forms')
+@section('main')
+<div class="mt-3">
+    <div id='calendar'></div>
+</div>
+
+<!--start overlay-->
+<div class="overlay toggle-menu"></div>
+@endsection
